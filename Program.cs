9abf1395d03be0c10ee985f821f09ea083b4aa25
Shibu Thannikkunnath <sthannikkunnath@salesforce.com>
2020-05-21 23:22:16 -0400

    Added logging aspect and implemented attribute

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace aopLogging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Adding 1+1, result is {Add(1,1)}");
+        }
+
+        [LoggingAspect]
+        static int Add(int a, int b){
+            return a+b;
         }
     }
 }
